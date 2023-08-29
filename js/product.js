d57e63d86cbb43a09b1be2661f73f5cdab6b83fd
eojin <aejin0501@naver.com>
2023-08-29 18:13:20 +0900

    판매페이지

--- js/product.js
+++ js/product.js
@@ -36,13 +36,20 @@ btn.addEventListener('click', function() {
 console.log('---------------------------------------')
 // 색상 선택 x 사이즈를 선택했을시 경고문
 const size = document.querySelectorAll('input[name="product_size"]')
+const u_size = document.querySelectorAll('label')
 const color = document.querySelector('#color_all')
-console.log(size, color)
+const order = document.querySelectorAll('.order')
+const product = document.querySelector('.order1')
+console.log(size, color, order, product, u_size)
+
+
 
 size.forEach(radioButton => {
     radioButton.addEventListener('click', function() {
         if (color.value === 'top1') {
             window.alert('색상을 선택해주세요.');
+        }else if (color.value <= 'top2'){
+            product.innerHTML = `데이 솔리드 카라 하프 티셔츠 그린 ${[]}`
         }
     });
 });
@@ -54,14 +61,15 @@ const minus = document.querySelector('#minus')
 const plus = document.querySelector('#plus')
 const quantity_value = document.querySelector('.quantity_value')
 const cash = document.querySelector('.value_cash')
-console.log(minus, plus, quantity_value, cash)
+const font = document.querySelector('#money > em')
+console.log(minus, plus, quantity_value, cash, font)
 
 let quantity = 0;
 let value_cash = 33000;
 
 function updateQuantityAndValue() {
     quantity_value.textContent = quantity;
-    cash.textContent = (quantity * value_cash) + ``;
+    cash.textContent = (quantity * value_cash).toLocaleString('ko-kr');
 }
 
 minus.addEventListener('click', function() {
@@ -75,6 +83,20 @@ plus.addEventListener('click', function() {
     quantity++;
     updateQuantityAndValue();
 });
+
+console.log('---------------------------------------')
+// 상품 이미지에 마우스를 올렸을 시 이미지 체인지
+
+const mainImg = document.querySelector('.s_left > img')
+const subImg = document.querySelectorAll('.s_btm > img')
+console.log(mainImg, subImg)
+
+subImg.forEach(subImg => {
+    subImg.addEventListener('mouseover',function() {
+        mainImg.src = subImg.src;
+        mainImg.alt = subImg.alt;
+    })
+})
 
 
 
