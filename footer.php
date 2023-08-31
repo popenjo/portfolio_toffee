5901e7dcf9675aa625a8ce9b222edc906bdf5ddf
popenjo <popenjo>
2023-09-01 00:13:57 +0900

    포트폴리오 수정/최종본

---
+++ footer.php
@@ -0,0 +1,17 @@
+    <!-- footer -->
+    <footer>
+        <div class="f_menu">
+            <a href="#">회사소개</a>
+            <a href="#">이용약관</a>
+            <a href="#">쇼핑몰 이용안내</a>
+            <a href="#">개인정보취급방침</a>
+            <a href="#">고객센터</a>
+        </div>
+        <address>
+            <p><em>COMPANY</em> 패션AFD <em>CEO</em>임수현 <em>Tel</em> 070-5223-2929</p><br>
+            <p><em>ADDRESS</em> 11901 경기도 구리시 갈매순환로166번길 46 (갈매동) 구리갈매금강펜테리움아이엑스타워 지식산업센터동 5층 511호 (갈매동)</p><br>
+            <p><em>BUSINESS NUMBER</em> 481-88-01924 <em>LICENSE</em> 2020-경기구리-0809 [사업자정보확인]</p><br>
+            <p><em>E-MAIL</em><a href="mailto:fashionafd@gmail.com">fashionafd@gmail.com</a></p> 
+        </address>
+        <p class="f_btn">COPYRIGHT ©패션AFD. ALL RIGHTS RESERVED.</p>
+    </footer>
